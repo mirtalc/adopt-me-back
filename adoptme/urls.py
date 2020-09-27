ad9a8c1c2fd04cac655ec4d8f614215255dd24ca
mirta.lopez <mirta.lopez@soprasteria.com>
2020-09-27 17:26:11 +0200

    feat: create and register test view

--- adoptme/urls.py
+++ adoptme/urls.py
@@ -19,6 +19,7 @@ from rest_framework import routers
 
 
 urlpatterns = [
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    # example_data = {
+    #     "Hello, world! You are at the api index"
+    # }
+    # example_status = status.HTTP_200_OK
+
+    # return Response(example_data, status=example_status)
+    return HttpResponse("Index view test, you are at the API!")
