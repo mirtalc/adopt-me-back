8b7d90bcced19765df0c80fc8feef5ec68a90785
mirta.lopez <mirta.lopez@soprasteria.com>
2020-09-27 16:05:12 +0200

    feat: install djangorestframework

--- adoptme/settings.py
+++ adoptme/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default installed apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom installed apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
